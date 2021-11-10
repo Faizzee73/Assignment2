f11845e627fdc236d5d8a1471f655356e379aee2
Faizzee73 <faizanhsn63@gmail.com>
2021-11-10 19:03:13 +0500

    main activity UI created

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private button move;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        move=findViewById(R.id.B1);
-
+        setContentView(R.layout.activity_main2);
     }
 }
